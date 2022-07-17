a8471edb0616e2f2e37482628fbfa09eb8d07c32
Kibe <smlkibe87@gmail.com>
2022-07-18 00:49:13 +0300

    Update individual (1).py

--- individual (1).py
+++ individual (1).py
@@ -5,13 +5,13 @@ import csv
  
 # Opening JSON file and loading the data
 # into the variable data
-with open('itumbid.json') as json_file:
+with open('#individual.json') as json_file:
     data = json.load(json_file)
  
 bytime_data = data['data']
  
 # now we will open a file for writing
-data_file = open('itumbid.csv', 'w', encoding="utf-8")
+data_file = open('#individual.csv', 'w', encoding="utf-8")
  
 # create the csv writer object
 csv_writer = csv.writer(data_file)
